012195d4e41f66237c635216212296b372d70f12
bandaid0625 <124765960+bandaid0625@users.noreply.github.com>
2023-03-21 10:56:28 +0900

    basic認証設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :set_time_zone, if: :user_signed_in?
 
   private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 
     def set_time_zone
       Time.zone = current_user.time_zone
